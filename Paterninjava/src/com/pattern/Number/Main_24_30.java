b443bc69b2b4f503132e80a605acfcb796d9d086
kanhaiyatiwari <tiwarikanhaiya149@gmail.com>
2023-09-28 13:06:28 +0530

    add

---
+++ Paterninjava/src/com/pattern/Number/Main_24_30.java
@@ -0,0 +1,29 @@
+package com.pattern.Number;
+
+public class Main_24_30 {
+public static void main(String[] args) {
+	p1() ;
+}
+public static void p1() {
+//	1 
+//	1 2 
+//	1 2 3 
+//	1 2 3 4 
+//	1 2 3 4 5 
+//	1 2 3 4 
+//	1 2 3 
+//	1 2 
+//	1
+	StringBuilder st=new StringBuilder();
+	for(int i=1;i<=5;i++) {
+		st.append(i+" ");
+		System.out.println(st);
+	}
+	
+	for(int i=0;i<5;i++) {
+		st.deleteCharAt(st.length()-1);
+		st.deleteCharAt(st.length()-1);
+		System.out.println(st);
+	}
+}
+}
