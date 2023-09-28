71f86bd9f010938768a48e2e80074aeb81145a97
kanhaiyatiwari <tiwarikanhaiya149@gmail.com>
2023-09-28 11:35:07 +0530

    add

--- Paterninjava/src/com/pattern/Main.java
+++ Paterninjava/src/com/pattern/Main.java
@@ -3,6 +3,40 @@ package com.pattern;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		patern1();
+	}
+	
+public static void patern1() {
+//	*
+//	**
+//	***
+//	****
+//	*****
+	String bag="";
+	for(int i=0;i<6;i++) { 
+		bag+="*";
+System.out.println(bag);
 	}
 }
+	
+	
+
+
+
+public static void patern2() {
+//	*
+//	**
+//	***
+//	****
+//	*****
+	String bag="";
+	for(int i=0;i<6;i++) { 
+		bag+="*";
+System.out.println(bag);
+	}
+}
+	
+	
+	
+}
+
